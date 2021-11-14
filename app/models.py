59ea5b1215b620cb806a741f8608f0e69a7cf5af
job karani <job.nyambura@student.moringaschool.com>
2021-11-14 11:33:17 +0300

    creates user class

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+from . import login_manager
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    hashed_pass = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    # blogs = db.relationship('Blog', backref='author', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.hashed_pass = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.hashed_pass, password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def __repr__(self):
+        return f'User {self.username}'
