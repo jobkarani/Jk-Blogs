ba2de352f39bb9162069ca6f03a11859855b6d9c
job karani <job.nyambura@student.moringaschool.com>
2021-11-16 16:18:13 +0300

    herokudeployment

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/developer/moringaschool/pitches/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Documents/flask/jk-blogs/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
