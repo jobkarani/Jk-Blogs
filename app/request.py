d200a50270ed9bd15d67dac4e8a995babf2c898e
job karani <job.nyambura@student.moringaschool.com>
2021-11-16 18:45:13 +0300

    deployment to herokuuuuuuuuuuuuuuu

--- app/request.py
+++ app/request.py
@@ -6,7 +6,7 @@ from .models import Quote
 
 # function to get quote from API
 def get_quote():
-    response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    response = request.get('http://quotes.stormconsultancy.co.uk/random.json')
     if response.status_code == 200:
         quote = response.json()
         print(quote)
