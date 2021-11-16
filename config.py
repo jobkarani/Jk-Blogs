8c388a958f0dc20fef2a2b095fffbf0eb0e90f03
job karani <job.nyambura@student.moringaschool.com>
2021-11-16 18:34:43 +0300

    deployment to herokuuuuuuuuuuuuu

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ class TestConfig(Config):
     pass
 
 
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URI .startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI  = SQLALCHEMY_DATABASE_URI .replace("postgres://", "postgresql://", 1)
 
 
 class DevConfig(Config):
